4de7fc9464a67acf1dddecd2638fd4df2778ff2d
Sahil1222hyper <pawarsahi2002@gmail.com>
2026-01-11 13:04:48 +0530

    Learning cohort

---
+++ main.js
@@ -0,0 +1,46 @@
+// greeting by personal name and lastname 
+// let firstName = 'Sahil';
+// let lastName = 'pawar';
+// console.log(`hello ${firstName} ${lastName} welcome to our hotel`)
+
+// greeting by gender 
+// let gender = 'women';
+// if (gender === 'man'){
+//     console.log("welcome sir to our hotel")
+// }else if(gender === 'women'){
+//     console.log("welcome mam to our hotel")
+// }
+
+// print 0-1000
+// let ans = 0;
+// for (let i = 0; i < 1001; i++){
+//     ans = ans + i; 
+// }
+// console.log(ans)
+
+// Even number print in the array
+// let arr = [1, 2, 3, 4, 22, 52, 6, 7, 8, 9, 10]
+// for (i = 0; i < arr.length; i++){
+//     if (arr[i] % 2 == 0){
+//         console.log(arr[i])
+//     }
+// }
+
+// Creating object 
+let person = [{
+    firstName : 'sahil',
+    lastName : 'pawar',
+    gender : 'male',
+    age : 23,
+},{
+    firstName : 'priya',
+    lastName : 'patil',
+    gender : 'female',
+    age : 22,
+},{
+    firstName : 'rudra',
+    lastName : 'salunke',
+    gender : 'male',
+    age : 25,
+}]
+console.log(person[2])
